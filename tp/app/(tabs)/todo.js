aca4bbe42b04ff32d2d34bef6684a422183a1797
Marty Rabatel <marty.rabatel2@gmail.com>
2024-06-04 13:34:33 +0200

    ToDo
    Add todo list feature

--- tp/app/(tabs)/todo.js
+++ tp/app/(tabs)/todo.js
@@ -1,10 +1,40 @@
-import { Text, View, StyleSheet, TextInput } from "react-native";
+import { Text, View, StyleSheet, TextInput, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Button } from "react-native-elements";
 
 export default function ToDoScreen() {
+  const [state, setState] = useState("");
+
+  const [data, setData] = useState([]);
+
+  function add() {
+    if (state === "") {
+      alert("Input is empty !");
+    } else if (data.findIndex((e) => state === e) === -1) {
+      setData((data) => [...data, state]);
+    } else {
+      alert("Already exist");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Todo</Text>
-      <TextInput style={styles.input} placeholder="Nom" />
+      <TextInput
+        style={styles.input}
+        placeholder="Nom"
+        onChangeText={setState}
+      />
+      <Button type="submit" onPress={add} title="Submit" />
+
+      <FlatList
+        data={data}
+        renderItem={(itemData) => (
+          <View>
+            <Text>{itemData.item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
